230a0cd25bd41c9493ed4aabb6a636466bd7ecdb
getwgm <zjoudqxjt@126.com>
2019-05-12 18:45:24 +0800

    init

---
+++ pages/login/login.js
@@ -0,0 +1,10 @@
+function redirectToHome() {
+    window.location.href = '../home/home.html'
+}
+var loginBtn = document.querySelector('#login-btn')
+loginBtn.onclick = function() {
+    
+}
+loginBtn.addEventListener('click', function() {
+    // 
+})
